3043faf96e0eb58b402998be983e3fe4cda5f65b
Minh Vu <mbvu@ucsd.edu>
2018-05-25 17:07:29 -0700

    Starting project...

---
+++ web.js
@@ -0,0 +1,19 @@
+const average = require('image-average-color');
+var express = require('express');
+var app = express();
+
+const port = 443;
+var rgb;
+
+app.get('/', function(req, res) {
+    average('image.jpg', (err, color) => {
+        if (err) throw err;
+        rgb = color;
+        res.send('Hello World' + rgb.toString());
+    });
+
+})
+
+app.listen(port, function(server) {
+    console.log(`Example app listening on port ${port}`);;
+})
